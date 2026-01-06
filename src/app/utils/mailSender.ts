b6234882b328935339cb7fc447864622698950b8
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2026-01-06 17:16:21 +0600

    updated

--- src/app/utils/mailSender.ts
+++ src/app/utils/mailSender.ts
@@ -3,56 +3,70 @@ import config from '../config'
 
 
 
+
+// Go Daddy business email setup for the node mailer
+export const sendEmail = async (to: string, subject: string, html: string) => {
+
+  
+  const transporter = nodemailer.createTransport({
+    host: 'smtpout.secureserver.net',
+    port: 465,
+    secure: true, // SSL/TLS
+    auth: {
+      user: config.nodemailer_host_email, 
+      pass: config.nodemailer_host_pass,  // SMTP app password if needed
+    },
+  });
+
+  try {
+    console.log('mail send started');
+    await transporter.sendMail({
+      from: config.nodemailer_host_email,
+      to,
+      subject,
+      text: '',
+      html,
+    });
+    console.log('mail sent successfully');
+  } catch (error) {
+    console.error('send mail error:', error);
+  }
+};
+
+
+
 // export const sendEmail = async (to: string, subject: string, html: string) => {
 
-//   console.log({
-//     NODEMAILER_HOST_EMAIL: process.env.NODEMAILER_HOST_EMAIL,
-//     NODEMAILER_HOST_PASS: process.env.NODEMAILER_HOST_PASS,
-//   });
 //   const transporter = nodemailer.createTransport({
-//     host: 'smtp.titan.email',
-//     port: 465,
-//     secure: true,
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: config.NODE_ENV === 'production',
 //     auth: {
-//       user: process.env.NODEMAILER_HOST_EMAIL!,
-//       pass: process.env.NODEMAILER_HOST_PASS!,
+//       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+//       user: config.nodemailer_host_email,
+//       pass: config.nodemailer_host_pass,
 //     },
 //   });
 
+  
 //   try {
-//         await transporter.verify(); // 👈 IMPORTANT
-//     console.log('✅ SMTP connection verified');
-//   } catch (error) {
-//     console.error('not verify it =>>>> :', error);
-//   }
-
-//   try {
-
+//     console.log('mail send started');
 //     await transporter.sendMail({
-//       from: `"StaffSecure" <karl.fairbrother@staffsecure.ai>`,
-//       to,
+//       from: 'team.robust.dev@gmail.com', // sender address
+//       to, // list of receivers
 //       subject,
-//       html,
+//       text: '', // plain text body
+//       html, // html body
 //     });
-
-//     console.log('✅ Mail sent successfully');
+    
+//     console.log('mail sended successfully');
+    
 //   } catch (error) {
-//     console.error('❌ Send mail error:', error);
+//     console.log('send mail error:', error);
+    
 //   }
+//   console.log('mail sended stopped');
 // };
-
-export const sendEmail = async (to: string, subject: string, html: string) => {
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: config.NODE_ENV === 'production',
-    auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: config.nodemailer_host_email,
-      pass: config.nodemailer_host_pass,
-    },
-  });
 
 // const transporter = nodemailer.createTransport({
 //   host: "mail.websupport.sk", // sending SMTP server
@@ -66,22 +80,3 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
 //     rejectUnauthorized: false  // ⚠️ disables certificate validation
 //   }
 // });
-
-  try {
-     console.log('mail send started');
-    await transporter.sendMail({
-      from: 'team.robust.dev@gmail.com', // sender address
-      to, // list of receivers
-      subject,
-      text: '', // plain text body
-      html, // html body
-    });
-
-    console.log('mail sended successfully');
-
-  } catch (error) {
-    console.log('send mail error:', error);
-
-  }
-  console.log('mail sended stopped');
-};
